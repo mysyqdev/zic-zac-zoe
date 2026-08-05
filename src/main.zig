7de494cd476df383d358495cfbae5599e3770342
Zhansen Zhalel <zhzhzh1806@gmail.com>
2026-08-05 16:55:58 +0500

    implement table printing

--- src/main.zig
+++ src/main.zig
@@ -3,12 +3,55 @@ const Io = std.Io;
 
 const zic_zac_zoe = @import("zic_zac_zoe"); // maybe will be used
 
+const Cell = enum { x, o };
+
 pub fn main(init: std.process.Init) !void {
     const io = init.io;
+    const table: [3][3]?Cell = .{
+        .{ null, null, null },
+        .{ null, .x, null },
+        .{ null, null, null },
+    };
 
     var stdout_buffer: [1024]u8 = undefined;
     var stdout_file_writer: Io.File.Writer = .init(.stdout(), io, &stdout_buffer);
     const stdout_writer = &stdout_file_writer.interface;
-    try stdout_writer.print("salem alem!\n", .{});
+    try stdout_writer.print(
+        \\   a b c
+        \\ 1 {u}|{u}|{u}
+        \\ 2 {u}|{u}|{u}
+        \\ 3 {u}|{u}|{u}
+    , tableToTuple(table));
+    try stdout_writer.print("\n", .{});
     try stdout_writer.flush();
+}
+
+fn tableToTuple(table: [3][3]?Cell) struct { u8, u8, u8, u8, u8, u8, u8, u8, u8 } {
+    var letters: [9]u8 = @splat(0);
+
+    for (table, 0..) |row, y| {
+        for (row, 0..) |letter, i| {
+            const index = i + (y * 3);
+            if (letter) |l| {
+                switch (l) {
+                    .x => letters[index] = 'x',
+                    .o => letters[index] = 'o',
+                }
+            } else {
+                letters[index] = ' ';
+            }
+        }
+    }
+
+    return .{
+        letters[0],
+        letters[1],
+        letters[2],
+        letters[3],
+        letters[4],
+        letters[5],
+        letters[6],
+        letters[7],
+        letters[8],
+    };
 }
